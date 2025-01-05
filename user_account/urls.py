60c91922c844133d3d5a672e265ce8c416aa9eb9
homer299 <igorteixeira2305@gmail.com>
2025-01-05 01:59:06 +0100

    password update system

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 import user_account.views as views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
 path("create_user_account/",views.UserAccountRegistrationView,name='create_user_account'),
 path("login_user_account/",views.UserAccountLoginView,name='login_user_account'),
 path("logout_user_account/",views.UserAccountLogoutView,name='logout_user_account'),
+
+#Password Change
+path('user_account/password_change/', auth_views.PasswordChangeView.as_view(template_name='user_account/password_change.html'),name='password_change'),
+
+path('user_account/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='user_account/password_change_done.html'), name='password_change_done'),
 
 ]
--- user_account/views.py
+++ user_account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
+
 from django.conf import settings
 from user_account.forms import UserAccoutRegistrationForm, UserAccountAuthenticationForm
 from user_account.models import UserAccount
