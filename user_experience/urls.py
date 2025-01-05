562a90595ac36b5e27ac6c0e7a30ec6cb0ac3bc6
homer299 <igorteixeira2305@gmail.com>
2025-01-05 14:44:48 +0100

    profile page created and states of self defined

--- user_experience/urls.py
+++ user_experience/urls.py
@@ -5,4 +5,5 @@ import user_experience.views as views
 urlpatterns = [
 path("",views.home_view,name='home'),
 path("searching/",views.searching_view,name='searching'),
+path("user_account_profile/<user_id>",views.user_account_profile_view,name='user_account_profile'),
 ]
--- user_experience/views.py
+++ user_experience/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
+from user_account.models import UserAccount
 import datetime
 
 
@@ -12,3 +13,35 @@ def home_view(request, *args, **kwargs):
 
 def searching_view(request):
     return render(request,'user_experience/searching.html')
+
+
+def user_account_profile_view(request, *args, **kwargs):
+    context = {}
+    user = request.user#Current logged account
+    is_self = True
+    user_id = kwargs.get("user_id")#Getting user id from the url
+
+    try:
+        account = UserAccount.objects.get(pk=user_id)#getting the account with id as in the url(which the profile page belongs to)
+        print("=============Got User object by pk/user_id =========")
+        print(account)
+        print("====================================================")
+    except UserAccount.DoesNotExist :
+        return HttpResponse("Something went wrong.")
+
+    if user.is_authenticated and user != account or not user.is_authenticated:
+        is_self = False
+        print("=============Different User/Not Auth=========================")
+        print(user.is_authenticated)
+        print(user)
+        print("=============================================================")
+
+
+    if account:
+        context['id'] = account.id
+        context['username'] = account.username
+        context['email'] = account.email
+
+        # Set the template variables to the values
+        context['is_self'] = is_self
+        return render(request, "user_experience/user_account_profile.html", context)
